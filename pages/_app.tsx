10c88b2b0f333728e79e8478726f8cfe7d148c15
riciemounti <babtdaddy67@gmail.com>
2021-11-23 12:22:31 +0300

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+// _app.tsx
+
+import { Provider } from 'next-auth/client';
+import { AppProps } from 'next/app';
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <Provider session={pageProps.session}>
+      <Component {...pageProps} />
+    </Provider>
+  );
+};
+
+export default App;
